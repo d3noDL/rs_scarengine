873de63a8cb373240f35a1001b1e30e4f85cb8ef
d3noDL <dino@adenogame.com>
2023-10-06 03:10:03 +0200

    Reformatted code

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,21 @@ use macroquad::prelude::*;
 async fn main() {
     //* pre loop vars and logic go here */
     loop {
-        clear_background(Color { r: 100.0 / 255., g: 149. / 255., b: 237. / 255., a: 255. / 255. });
+        clear_background(Color {
+            r: 100.0 / 255.0,
+            g: 149.0 / 255.0,
+            b: 237.0 / 255.0,
+            a: 1.0,
+        });
         //* main loop logic goes here */
 
-            draw_text("SCARENGINE 0.1", screen_width() / 2. - 140., screen_height() / 2., 48., WHITE);
+        draw_text(
+            "SCARENGINE 0.1",
+            screen_width() / 2.0 - 140.0,
+            screen_height() / 2.0,
+            48.0,
+            WHITE
+        );
 
         //* main loop logic ends here */
         next_frame().await;
